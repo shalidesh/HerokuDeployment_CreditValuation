b10bf2aeae8424eeff9a27973d80c90322129d41
shalidesh <shalidesh970@gmail.com>
2023-10-17 09:17:23 +0530

    sixth commit

--- app.py
+++ app.py
@@ -71,8 +71,9 @@ def predict_datapoint():
 
 
         # return {"Success": finalPred}
-    
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)        
+        
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)      
 
